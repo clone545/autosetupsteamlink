997d0d15f65c1b0f69966b37d7bb9472a21fa26b
clone545 <clone545@users.noreply.github.com>
2020-04-23 15:12:33 +0100

    Create autosetupsteamlink.sh

---
+++ autosetupsteamlink.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Install Steamlink
+sudo apt-get update && sudo apt-get install steamlink -y 
+
+#Set Steamlink to autostart
+echo -n "Steamlink" >> ~/.config/lxsession/LXDE-pi/autostart
+
+#Install and set up the fan shim
+git clone https://github.com/pimoroni/fanshim-python
+sudo /fanshim-python/install.sh
+sudo /fanshim-python/examples/install-service.sh --on-threshold 65 --off-threshold 55 --delay 2
